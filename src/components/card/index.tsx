768290e2ae752b3b00fe08883d6579eb7ac4d648
jer3m01 <jer3m01@jer3m01.com>
2022-12-12 15:38:56 +0100

    style: format

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -6,28 +6,26 @@ import { Text } from "@jundao/design";
 export type CardProps = JSX.IntrinsicElements["div"] & {
 	title?: JSX.Element;
 	size?: "small" | "default";
-  noPadding?: boolean;
+	noPadding?: boolean;
 };
 
 export default function Card(props: CardProps) {
-	const [{ title, size = "default", children, noPadding = false }, others] = splitProps(props, [
-		"title",
-		"size",
-		"children",
-    "noPadding",
-	]);
+	const [{ title, size = "default", children, noPadding = false }, others] =
+		splitProps(props, ["title", "size", "children", "noPadding"]);
 
 	return (
 		<div
 			class="jdd card"
 			classList={{
 				small: size === "small",
-        "no-padding": noPadding,
+				"no-padding": noPadding,
 			}}
 			{...others}
 		>
 			<Show when={title}>
-        <div class="title">{typeof title === "string" ? <Text>{title}</Text> : title }</div>
+				<div class="title">
+					{typeof title === "string" ? <Text>{title}</Text> : title}
+				</div>
 			</Show>
 			<div class="content">{children}</div>
 		</div>
