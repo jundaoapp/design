0107b1694e287f6f7b28c3ed1e76825b529c81fe
jer3m01 <jer3m01@jer3m01.com>
2023-01-26 21:04:39 +0100

    fix: build

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,40 @@
 import withSolid from "rollup-preset-solid";
-import scss from "rollup-plugin-scss";
+import styles from "rollup-plugin-styles";
+import glob from "glob";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-export default withSolid({
-	input: "src/components/index.ts",
-	targets: ["esm", "cjs"],
-	plugins: [scss()],
-});
+export default [
+	withSolid({
+		input: "src/components/index.tsx",
+		targets: ["esm", "cjs"],
+		plugins: [
+			styles({
+				mode: "extract",
+			}),
+		],
+	}),
+	{
+		input: Object.fromEntries(
+			glob.sync("src/components/**/index.scss").map((file) => [
+				// This remove `src/` as well as the file extension from each
+				// file, so e.g. src/nested/foo.js becomes nested/foo
+				path.relative(
+					"src",
+					file.slice(0, file.length - path.extname(file).length),
+				),
+				// This expands the relative paths to absolute paths, so e.g.
+				// src/nested/foo becomes /project/src/nested/foo.js
+				fileURLToPath(new URL(file, import.meta.url)),
+			]),
+		),
+		output: {
+			dir: "dist_temp/source",
+		},
+		plugins: [
+			styles({
+				mode: "extract",
+			}),
+		],
+	},
+];
