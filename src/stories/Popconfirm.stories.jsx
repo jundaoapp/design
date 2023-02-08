eb762549e1980edf187a1983f0c3535981f351fd
jer3m01 <jer3m01@jer3m01.com>
2023-02-08 23:11:16 +0100

    wip

--- src/stories/Popconfirm.stories.jsx
+++ src/stories/Popconfirm.stories.jsx
@@ -4,7 +4,6 @@ import { action } from "@storybook/addon-actions";
 export default {
 	title: "Components/Popconfirm",
 	argTypes: {
-		onClick: { action: "onClick" },
 		type: {
 			control: { type: "select" },
 			options: ["info", "danger", "warning"],
