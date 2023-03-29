106b9ae83141808bfacfc428ba3dc0da544f06c2
jer3m01 <jer3m01@jer3m01.com>
2023-03-29 22:45:28 +0200

    wip

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,172 +1,244 @@
 import "./index.scss";
-import { IntrinsicComponentProps } from "../types";
-import { processProps } from "../utilities";
-import { combineProps } from "@solid-primitives/props";
+import {IntrinsicComponentProps} from "../types";
+import {processProps} from "../utilities";
+import {combineProps} from "@solid-primitives/props";
 import LayoutHeader from "./header";
 import LayoutSidebar from "./sidebar";
 import LayoutContent from "./content";
 import LayoutFooter from "./footer";
-import { LayoutContextProvider, useLayoutContext } from "./context";
-import { createSignal, Show } from "solid-js";
-import { createBreakpoints } from "@solid-primitives/media";
-import { WindowEventListener } from "@solid-primitives/event-listener";
+import {LayoutContextProvider, useLayoutContext} from "./context";
+import {createEffect, createSignal, Show} from "solid-js";
+import {createBreakpoints} from "@solid-primitives/media";
+import {WindowEventListener} from "@solid-primitives/event-listener";
 
 export type LayoutProps = IntrinsicComponentProps<
-	"div",
-	{
-		mobileWidthBreakpoint?: string;
-	}
+  "div",
+  {
+    mobileWidthBreakpoint?: string;
+  }
 >;
 
 function Layout(props: LayoutProps) {
-	return (
-		<LayoutContextProvider value={undefined}>
-			<InnerLayout {...props} />
-		</LayoutContextProvider>
-	);
+  return (
+    <LayoutContextProvider value={undefined}>
+      <InnerLayout {...props} />
+    </LayoutContextProvider>
+  );
 }
 
 function InnerLayout(props: LayoutProps) {
-	const [local, others] = processProps({
-		props,
-		default: {
-			mobileWidthBreakpoint: "600px",
-		},
-		keys: ["children", "mobileWidthBreakpoint"],
-	});
+  const [local, others] = processProps({
+    props,
+    default: {
+      mobileWidthBreakpoint: "600px",
+    },
+    keys: ["children", "mobileWidthBreakpoint"],
+  });
 
-	const breakpoints = createBreakpoints({
-		desktop: local.mobileWidthBreakpoint,
-	});
+  const breakpoints = createBreakpoints({
+    desktop: local.mobileWidthBreakpoint,
+  });
 
-	let ref!: HTMLDivElement;
+  let ref!: HTMLDivElement;
 
-	const [swipeStart, setSwipeStart] = createSignal({ x: 0, y: 0 });
-	const [leftSidebarOffset, setLeftSidebarOffset] = createSignal(0);
-	const [rightSidebarOffset, setRightSidebarOffset] = createSignal(0);
-	const [sidebarTransition, setSidebarTransition] = createSignal("");
-	const [leftSidebarOpen, setLeftSidebarOpen] = createSignal(false);
-	const [rightSidebarOpen, setRightSidebarOpen] = createSignal(false);
+  const [swipeStart, setSwipeStart] = createSignal({x: 0, y: 0});
+  const [leftSidebarOffset, setLeftSidebarOffset] = createSignal(0);
+  const [rightSidebarOffset, setRightSidebarOffset] = createSignal(0);
+  const [sidebarTransition, setSidebarTransition] = createSignal(false);
+  const [leftSidebarOpen, setLeftSidebarOpen] = createSignal(false);
+  const [rightSidebarOpen, setRightSidebarOpen] = createSignal(false);
 
-	const combinedProps = combineProps(others, {
-		ref: (el) => (ref = el),
-		class: "jdd layout",
-		get classList() {
-			return {
-				mobile: !breakpoints.desktop,
-				"open-left": leftSidebarOpen(),
-				"open-right": rightSidebarOpen(),
+  createEffect(() => {
+    if (leftSidebarOpen() || rightSidebarOpen()) document.body.classList.add("jdd-sidebar-open");
+    else document.body.classList.remove("jdd-sidebar-open");
+  });
+
+  const combinedProps = combineProps(others, {
+    ref: (el) => (ref = el),
+    class: "jdd layout",
+    get classList() {
+      return {
+        mobile: !breakpoints.desktop,
+        "open-left": leftSidebarOpen(),
+        "open-right": rightSidebarOpen(),
+      };
+    },
+    get style() {
+      return {
+        "--jdd-left-sidebar-offset": `${leftSidebarOffset()}px`,
+        "--jdd-right-sidebar-offset": `${rightSidebarOffset()}px`,
+        "--jdd-sidebar-transition": sidebarTransition() ? "transform 0.3s var(--jdd-transition-function), opacity 0.3s var(--jdd-transition-function)" : "",
+        get ["--jdd-layout-backdrop-opacity"]() {
+          const leftSidebar = ref.querySelector<HTMLElement | null>(".layout-sidebar:first-child");
+          if (leftSidebar && leftSidebarOffset() > 0) {
+            return leftSidebarOffset() / leftSidebar.offsetWidth;
+          }
+
+          const rightSidebar = ref.querySelector<HTMLElement | null>(".layout-sidebar:last-child");
+          if (rightSidebar && rightSidebarOffset() > 0) {
+            return rightSidebarOffset() / rightSidebar.offsetWidth;
+          }
+
+          return "";
+        },
+      };
+    },
+  });
+
+  const context = useLayoutContext();
+
+  const [getContent] = context.content;
+  const [getSidebarLeft] = context.sidebarLeft;
+  const [getSidebarRight] = context.sidebarRight;
+
+  const toucheMoveHandler = (e: TouchEvent) => {
+    const swipeEnd = {
+      x: e.touches[0].clientX,
+      y: e.touches[0].clientY,
+    };
+
+		if (leftSidebarOpen() || rightSidebarOpen()) {
+			const swipeDiff = {
+				x: Math.abs(swipeEnd.x - swipeStart().x),
+				y: Math.abs(swipeEnd.y - swipeStart().y),
 			};
-		},
-		get style() {
-			return {
-				"--jdd-left-sidebar-offset": `${leftSidebarOffset()}px`,
-				"--jdd-right-sidebar-offset": `${rightSidebarOffset()}px`,
-				"--jdd-sidebar-transition": sidebarTransition(),
-			};
-		},
-	});
 
-	const context = useLayoutContext();
+			if (swipeDiff.x < swipeDiff.y * 2) return;
 
-	const [getContent] = context.content;
-	const [getSidebarLeft] = context.sidebarLeft;
-	const [getSidebarRight] = context.sidebarRight;
+			if (leftSidebarOpen() && swipeEnd.x < swipeStart().x) {
+					setLeftSidebarOffset(swipeDiff.x);
+				return;
+			}
 
-	return (
-		<div {...combinedProps}>
-			{local.children}
-			<div class="layout-main">
-				<Show when={getSidebarLeft()} keyed>
-					{(sidebarLeft) => sidebarLeft}
-				</Show>
-				<Show when={getContent()} keyed>
-					{(content) => content}
-				</Show>
-				<Show when={getSidebarRight()} keyed>
-					{(sidebarRight) => sidebarRight}
-				</Show>
-			</div>
+			if (rightSidebarOpen() && swipeEnd.x > swipeStart().x) {
+					setRightSidebarOffset(swipeDiff.x);
+				return;
+			}
+			return;
+		}
 
-			<Show when={!breakpoints.desktop}>
-				<WindowEventListener
-					onTouchstart={(e) =>
-						setSwipeStart({
-							x: e.touches[0].clientX,
-							y: e.touches[0].clientY,
-						})
-					}
-					onTouchmove={(e) => {
-						const swipeEnd = {
-							x: e.touches[0].clientX,
-							y: e.touches[0].clientY,
-						};
+    let swipeDirection: "left" | "right" | undefined;
 
-						let swipeDirection: "left" | "right" | undefined;
+    // Left swipe
+    if (swipeStart().x < 50) {
+      swipeDirection = "right";
+    }
 
-						// Left swipe
-						if (swipeStart().x < 50) {
-							swipeDirection = "right";
-						}
+    // Right swipe
+    if (window.innerWidth - swipeStart().x < 50) {
+      swipeDirection = "left";
+    }
 
-						// Right swipe
-						if (window.innerWidth - swipeStart().x < 50) {
-							swipeDirection = "left";
-						}
+    if (swipeDirection === undefined) return;
 
-						if (swipeDirection === undefined) return;
+    const swipeDiff = {
+      x: Math.abs(swipeEnd.x - swipeStart().x),
+      y: Math.abs(swipeEnd.y - swipeStart().y),
+    };
 
-						const swipeDiff = {
-							x: Math.abs(swipeEnd.x - swipeStart().x),
-							y: Math.abs(swipeEnd.y - swipeStart().y),
-						};
+    // Threshold swipe orientation
+    if (swipeDiff.x < swipeDiff.y * 2) return;
 
-						// Threshold swipe orientation
-						if (swipeDiff.x < swipeDiff.y * 2) return;
+    document.body.style.touchAction = "none";
+    document.body.style.userSelect = "none";
+    document.body.style.overflowY = "hidden";
 
-						if (swipeDirection === "right") {
-							setLeftSidebarOffset(swipeDiff.x);
-						} else {
-							setRightSidebarOffset(swipeDiff.x);
-						}
-					}}
-					onTouchend={() => {
-						setSidebarTransition(
-							"transform 0.3s var(--jdd-transition-function)",
-						);
-						setTimeout(() => {
-							setSidebarTransition("");
-						}, 300);
+    if (swipeDirection === "right") {
+      if (!rightSidebarOpen()) setLeftSidebarOffset(swipeDiff.x);
+    } else {
+      if (!leftSidebarOpen()) setRightSidebarOffset(swipeDiff.x);
+    }
+  }
 
-						console.log(leftSidebarOffset(), rightSidebarOffset());
+  const touchEndHandler = () => {
+		console.log(leftSidebarOpen(), leftSidebarOffset(), rightSidebarOpen(), rightSidebarOffset())
+		if (leftSidebarOffset() + rightSidebarOffset() > 0) {
+			setSidebarTransition(true);
+		}
 
-						if (leftSidebarOffset() > 75) {
-							setLeftSidebarOpen(true);
-						}
+		setTimeout(() => {
+			setLeftSidebarOffset(0);
+			setRightSidebarOffset(0);
+		});
 
-						if (rightSidebarOffset() > 75) {
-							setRightSidebarOpen(true);
-						}
+		setTimeout(() => {
+			setSidebarTransition(false);
+			document.body.style.touchAction = "";
+			document.body.style.userSelect = "";
+			document.body.style.overflowY = "";
+			}, 300);
 
-						setTimeout(() => {
-							setLeftSidebarOffset(0);
-							setRightSidebarOffset(0);
-						});
-					}}
-				/>
-			</Show>
-		</div>
-	);
+		if (leftSidebarOpen() && leftSidebarOffset() > 75) {
+			setLeftSidebarOpen(false);
+			return;
+		}
+
+		if (rightSidebarOpen() && rightSidebarOffset() > 75) {
+			setRightSidebarOpen(false);
+			return;
+		}
+
+    if (!rightSidebarOpen() && leftSidebarOffset() > 75) {
+      setLeftSidebarOpen(true);
+    }
+
+    if (!leftSidebarOpen() && rightSidebarOffset() > 75) {
+      setRightSidebarOpen(true);
+    }
+  }
+
+  return (
+    <div {...combinedProps}>
+      {local.children}
+      <div class="layout-main">
+        <Show when={getSidebarLeft()} keyed>
+          {(sidebarLeft) => sidebarLeft}
+        </Show>
+        <Show when={getContent()} keyed>
+          {(content) => content}
+        </Show>
+        <Show when={getSidebarRight()} keyed>
+          {(sidebarRight) => sidebarRight}
+        </Show>
+      </div>
+
+      <Show when={!breakpoints.desktop}>
+        <div class="layout-backdrop" onclick={() => {
+          setSidebarTransition(true);
+
+          setTimeout(() => {
+            setLeftSidebarOpen(false);
+            setRightSidebarOpen(false);
+          });
+
+          setTimeout(() => {
+            setSidebarTransition(false);
+          }, 300);
+        }}/>
+
+        <WindowEventListener
+          onTouchstart={(e) =>
+            setSwipeStart({
+              x: e.touches[0].clientX,
+              y: e.touches[0].clientY,
+            })
+          }
+          onTouchmove={toucheMoveHandler}
+          onTouchend={touchEndHandler}
+        />
+      </Show>
+    </div>
+  );
 }
 
 const CompoundedLayout = Layout as typeof Layout & {
-	Header: typeof LayoutHeader;
-	Sidebar: typeof LayoutSidebar;
-	Content: typeof LayoutContent;
-	Footer: typeof LayoutFooter;
+  Header: typeof LayoutHeader;
+  Sidebar: typeof LayoutSidebar;
+  Content: typeof LayoutContent;
+  Footer: typeof LayoutFooter;
 };
 CompoundedLayout.Header = LayoutHeader;
 CompoundedLayout.Sidebar = LayoutSidebar;
 CompoundedLayout.Content = LayoutContent;
 CompoundedLayout.Footer = LayoutFooter;
-export { CompoundedLayout as Layout };
+export {CompoundedLayout as Layout};
--- src/stories/Layout.stories.jsx
+++ src/stories/Layout.stories.jsx
@@ -1,161 +1,159 @@
-import { Layout } from "@jundao/design";
+import {Layout} from "@jundao/design";
 
 export default {
-	title: "Components/Layout",
-	argTypes: {},
+  title: "Components/Layout",
+  argTypes: {},
 };
 
 const Template = (props) => {
-	return (
-		<Layout style={{ "min-width": "90vw", "min-height": "90vh" }}>
-			<Layout.Header
-				style={{
-					display: "flex",
-					"justify-content": "center",
-					"align-items": "center",
-					background: "var(--jdd-blue-3)",
-					padding: "1rem",
-				}}
-			>
-				Header
-			</Layout.Header>
-			<Layout.Sidebar
-				style={{
-					display: "flex",
-					"justify-content": "center",
-					"align-items": "center",
-					background: "var(--jdd-blue-2)",
-					padding: "1rem",
-				}}
-			>
-				Sidebar Left
-			</Layout.Sidebar>
-			<Layout.Content
-				style={{
-					display: "flex",
-					"justify-content": "center",
-					"align-items": "center",
-					background: "var(--jdd-blue-4)",
-					padding: "1rem",
-				}}
-			>
-				Content
-			</Layout.Content>
-			<Layout.Sidebar
-				position="right"
-				style={{
-					display: "flex",
-					"justify-content": "center",
-					"align-items": "center",
-					background: "var(--jdd-blue-2)",
-					padding: "1rem",
-				}}
-			>
-				Sidebar Right
-			</Layout.Sidebar>
-			<Layout.Footer
-				style={{
-					display: "flex",
-					"justify-content": "center",
-					"align-items": "center",
-					background: "var(--jdd-blue-3)",
-					padding: "1rem",
-				}}
-			>
-				Footer
-			</Layout.Footer>
-		</Layout>
-	);
+  return (
+    <Layout style={{"min-width": "90vw", "min-height": "90vh"}}>
+      <Layout.Header
+        style={{
+          display: "flex",
+          "justify-content": "center",
+          "align-items": "center",
+          background: "var(--jdd-blue-3)",
+          padding: "1rem",
+        }}
+      >
+        Header
+      </Layout.Header>
+      <Layout.Sidebar
+        style={{
+          display: "flex",
+          "justify-content": "center",
+          "align-items": "center",
+          background: "var(--jdd-blue-2)",
+          padding: "1rem",
+        }}
+      >
+        Sidebar Left
+      </Layout.Sidebar>
+      <Layout.Content
+        style={{
+          display: "flex",
+          "justify-content": "center",
+          "align-items": "center",
+          background: "var(--jdd-blue-4)",
+          padding: "1rem",
+        }}
+      >
+        Content
+      </Layout.Content>
+      <Layout.Sidebar
+        position="right"
+        style={{
+          display: "flex",
+          "justify-content": "center",
+          "align-items": "center",
+          background: "var(--jdd-blue-2)",
+          padding: "1rem",
+        }}
+      >
+        Sidebar Right
+      </Layout.Sidebar>
+      <Layout.Footer
+        style={{
+          display: "flex",
+          "justify-content": "center",
+          "align-items": "center",
+          background: "var(--jdd-blue-3)",
+          padding: "1rem",
+        }}
+      >
+        Footer
+      </Layout.Footer>
+    </Layout>
+  );
 };
 
 export const Default = Template.bind({});
 Default.args = {};
 
 const ScrollTemplate = (props) => {
-	return (
-		<>
-			<style>
-				{`
+  return (
+    <>
+      <style>
+        {`
 			.sb-main-padded {
 			  padding: 0 !important;
 			}
 			`}
-			</style>
-			<div style={{ height: "100vh", width: "100vw", overflow: "scroll" }}>
-				<Layout>
-					<Layout.Header
-						style={{
-							display: "flex",
-							"justify-content": "center",
-							"align-items": "center",
-							background: "var(--jdd-blue-3)",
-							padding: "1rem",
-						}}
-					>
-						Header
-					</Layout.Header>
-					<Layout.Sidebar
-						style={{
-							display: "flex",
-							"justify-content": "center",
-							"align-items": "center",
-							background: "var(--jdd-blue-2)",
-							padding: "1rem",
-						}}
-					>
-						Sidebar Left
-					</Layout.Sidebar>
-					<Layout.Content
-						style={{
-							display: "flex",
-							"justify-content": "center",
-							"align-items": "center",
-							background: "var(--jdd-blue-4)",
-							padding: "1rem",
-							height: "200vh",
-						}}
-					>
-						Content
-					</Layout.Content>
-					<Layout.Sidebar
-						position="right"
-						style={{
-							display: "flex",
-							"justify-content": "center",
-							"align-items": "center",
-							background: "var(--jdd-blue-2)",
-							padding: "1rem",
-						}}
-					>
-						Sidebar Right
-					</Layout.Sidebar>
-					<Layout.Footer
-						style={{
-							display: "flex",
-							"justify-content": "center",
-							"align-items": "center",
-							background: "var(--jdd-blue-3)",
-							padding: "1rem",
-						}}
-					>
-						Footer
-					</Layout.Footer>
-				</Layout>
-			</div>
-		</>
-	);
+      </style>
+      <Layout>
+        <Layout.Header
+          style={{
+            display: "flex",
+            "justify-content": "center",
+            "align-items": "center",
+            background: "var(--jdd-blue-3)",
+            padding: "1rem",
+          }}
+        >
+          Header
+        </Layout.Header>
+        <Layout.Sidebar
+          style={{
+            display: "flex",
+            "justify-content": "center",
+            "align-items": "center",
+            background: "var(--jdd-blue-2)",
+            padding: "1rem",
+          }}
+        >
+          Sidebar Left
+        </Layout.Sidebar>
+        <Layout.Content
+          style={{
+            display: "flex",
+            "justify-content": "center",
+            "align-items": "center",
+            background: "var(--jdd-blue-4)",
+            padding: "1rem",
+            height: "200vh",
+          }}
+        >
+          Content
+        </Layout.Content>
+        <Layout.Sidebar
+          position="right"
+          style={{
+            display: "flex",
+            "justify-content": "center",
+            "align-items": "center",
+            background: "var(--jdd-blue-2)",
+            padding: "1rem",
+          }}
+        >
+          Sidebar Right
+        </Layout.Sidebar>
+        <Layout.Footer
+          style={{
+            display: "flex",
+            "justify-content": "center",
+            "align-items": "center",
+            background: "var(--jdd-blue-3)",
+            padding: "1rem",
+          }}
+        >
+          Footer
+        </Layout.Footer>
+      </Layout>
+    </>
+  );
 };
 
 export const Scroll = ScrollTemplate.bind({});
 Scroll.args = {};
 
 const MobileTemplate = (props) => {
-	return (
-		<iframe
-			src="/iframe.html?args=&id=components-layout--scroll&viewMode=story"
-			style={{ height: "90vh" }}
-		/>
-	);
+  return (
+    <iframe
+      src="/iframe.html?args=&id=components-layout--scroll&viewMode=story"
+      style={{height: "90vh"}}
+    />
+  );
 };
 
 export const Mobile = MobileTemplate.bind({});
